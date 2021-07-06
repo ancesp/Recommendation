8479db56bc11ff304ce511b76f9a2bfd014af121
Andrea Speidel <andrea.speidel@dxc.com>
2021-07-06 10:50:11 +0200

    fixes in recommendation algorithm

--- recommendation.py
+++ recommendation.py
@@ -10,7 +10,6 @@ import json
 product_data = pd.read_csv("payslips.csv")
 product_list = pd.read_csv("products.csv")
 
-
 # get parameters for algorithm
 df_shape = product_data.shape
 n_of_transactions = df_shape[0]
@@ -21,12 +20,12 @@ for i in range(0, n_of_transactions):
     records.append([])
     for j in range(0, n_of_products):
         if (str(product_data.values[i, j]) != 'nan'):
-            records[i].append(str(product_data.values[i, j]))
+            records[i].append(str(int(product_data.values[i, j])))
 
 # print(records)
 
 association_rules = apriori(
-    records, min_support=0.001, min_confidence=0.0, min_lift=0, max_length=2)
+    records, min_support=0.2, min_confidence=0.0, min_lift=1, max_length=2)
 association_results = list(association_rules)
 
 
@@ -34,7 +33,8 @@ association_results = list(association_rules)
 lookup_table = {}
 
 for index, item in product_list.iterrows():
-    lookup_table[item[0]] = []
+    # print(item[0])
+    lookup_table[str(item[0])] = []
 
 for item in association_results:
     pair = item[0]
@@ -48,12 +48,11 @@ for item in association_results:
 
     if(len(items) > 1):
 
-        print(type(items[1]))
         # when working with product names
         lookup_pair_1 = [items[1], item[2][0][2]]
         lookup_pair_2 = [items[0], item[2][0][2]]
-        lookup_table[items[0]].append(lookup_pair_1)
-        lookup_table[items[1]].append(lookup_pair_2)
+        lookup_table[str(items[0])].append(lookup_pair_1)
+        lookup_table[str(items[1])].append(lookup_pair_2)
 
     print(to_print)
     print("Support: " + str(item[1]))
@@ -67,18 +66,19 @@ for key, item in lookup_table.items():
     item = sorted(item, key=itemgetter(1), reverse=True)
     lookup_table[key] = item
 
+print(lookup_table)
+
 
 def getRecommendationsForProduct(product_id):
     recommended_products = []
-    for item in lookup_table[int(product_id)]:
-
+    for item in lookup_table[str(product_id)]:
         recommended_products.append(item[0])
 
     recommendations = json.dumps(recommended_products)
     return recommendations
 
 
-print(getRecommendationsForProduct(26))
+print(getRecommendationsForProduct(2))
 
 # call from backend
 app = flask.Flask(__name__)
